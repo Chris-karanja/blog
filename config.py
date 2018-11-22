8d87c8c6574dbb76a0c3c3a0c2289231d41f92a0
Chris-karanja <chriswainaina0@gmail.com>
2018-11-22 08:17:10 +0300

    added a db uri and SQAlchemy

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kobura:fbi321@localhost/bloggy'
 
 class ProdConfig(Config):
     pass
